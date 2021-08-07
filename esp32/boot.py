c4155173f708de26de54d0cede2d15f68e6a67a7
serhii-pedko <nitrotoll@gmail.com>
2021-08-07 23:41:29 +0300

    refactor boot process

--- esp32/boot.py
+++ esp32/boot.py
@@ -1,32 +1,86 @@
 import network
 import time
 import ubinascii
+import ujson
+import os
 from machine import Pin
-
-wifi_ssid = ''  # TODO add reading from file
-wifi_password = ''  # TODO add reading from file
 
 status_pin = Pin(5, Pin.OUT)
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 
-
-if not wlan.isconnected():
-    my_wifi_mac = ubinascii.hexlify(network.WLAN().config('mac'), ':').decode()
-    print(my_wifi_mac, 'connecting to network', wifi_ssid)
-    wlan.connect(wifi_ssid, wifi_password)
-    for x in range(1, 2):  # TODO add function for this
-        status_pin.off()
-        time.sleep_ms(250)
-        status_pin.on()
-        time.sleep_ms(250)
-    count = 0
-    while not wlan.isconnected() and count < 10:
-        for x in range(1, 5):  # TODO add function for this
+# Read config from config.json
+root_dir_listing = os.listdir()
+if 'config.json' in root_dir_listing:
+    config_file = open('config.json', 'r')
+    raw_config = config_file.read()
+    try:
+        config = ujson.loads(raw_config.replace('\n', ''))
+    except ValueError:
+        print('Incorrect format of json file')
+        while True:
             status_pin.off()
-            time.sleep_ms(100)
+            time.sleep(2)
+            for x in range(0, 4):
+                status_pin.on()
+                time.sleep_ms(500)
+                status_pin.off()
+                time.sleep_ms(500)
+else:
+    print('Json config file does not exist')
+    while True:
+        status_pin.off()
+        time.sleep(2)
+        for x in range(0, 2):
             status_pin.on()
-        count = count + 1
-        time.sleep_ms(500)
-    print('Connected to network', wifi_ssid, end='. ')
-    print('Network config:', wlan.ifconfig())
+            time.sleep_ms(500)
+            status_pin.off()
+            time.sleep_ms(500)
+
+if 'wifi' in config and len(config['wifi']):
+    for wifi_ssid, wifi_password in config['wifi'].items():
+        my_wifi_mac = ubinascii.hexlify(network.WLAN().config('mac'), ':').decode()
+        print(my_wifi_mac, 'connecting to network', wifi_ssid)
+        if not wlan.isconnected():
+            connect_attempt = 0
+            wlan.connect(wifi_ssid, wifi_password)
+            while not wlan.isconnected() and connect_attempt < 10:
+                connect_attempt = connect_attempt + 1
+                print("Attempt", connect_attempt)
+                for x in range(1, 5):  # TODO add function for this
+                    status_pin.on()
+                    time.sleep_ms(100)
+                    status_pin.off()
+                    time.sleep_ms(100)
+                time.sleep_ms(300)
+            if wlan.isconnected():
+                print('Connected to network', wifi_ssid)
+                print('Network config:', wlan.ifconfig())
+                status_pin.on()
+                break
+            else:
+                print('Connection to network', wifi_ssid, 'is not successful.')
+                wlan.disconnect()
+    if not wlan.isconnected():
+        print('Device can`t connect to any configured network.')
+        while True:
+            status_pin.off()
+            time.sleep(2)
+            for x in range(0, 3):
+                status_pin.on()
+                time.sleep_ms(500)
+                status_pin.off()
+                time.sleep_ms(500)
+else:
+    print('Cant get networks configuration.')
+    while True:
+        status_pin.off()
+        time.sleep(2)
+        for x in range(0, 1):
+            status_pin.on()
+            time.sleep_ms(500)
+            status_pin.off()
+            time.sleep_ms(500)
+
+# TODO Add OTA
+
--- esp32/readme.md
+++ esp32/readme.md
@@ -1,6 +1,15 @@
 # Early dev for ESP32
 ##### based on Mycropython
 
+## Led indicator states
+- instant light - ready
+- rapid blinking - connecting to Wi-Fi network
+- 1 slow blink - can`t get networks configuration
+- 2 slow blinks - json config file does not exist on device
+- 3 slow blinks - device can`t connect to any configured network
+- 4 slow blinks - incorrect format of json file
+- 5 slow blinks - 
+
 ## Dev env preparaions
 ### Install tools
 ```shell
@@ -22,3 +31,4 @@ picocom -b 115200 /dev/ttyUSB0
 ## Debug other services
 Also, you can use `main.py` in your PC python interpreter as software debug plug
 to debug without real ESP32 board
+
